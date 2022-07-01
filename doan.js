10603e1776a27b4a6264aa8b7fbd170d0d05318b
qui281194 <quicao902@gmail.com>
2022-07-01 22:10:57 +0700

    Initial commit

---
+++ doan.js
@@ -0,0 +1,5 @@
+var btnSearch = document.querySelector('.search-box__btn');
+btnSearch.addEventListener('click', function(){
+    this.parentElement.classList.toggle('open');
+    this.previousElementSibling.focus()
+})
